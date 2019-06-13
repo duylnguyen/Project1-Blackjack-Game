9cd4ef9454110a4ed1ae9e5c8f0647700d82bb98
Duy Nguyen <duy_nobita@icloud.com>
2019-06-13 19:50:46 -0400

    Fix shuffle issue

--- main.js
+++ main.js
@@ -1,40 +1,36 @@
 $(document).ready(function() {
 
+    dealerHand = [];
+    playerHand = [];
+    dealerHandValue = 0;
+    playerHandValue = 0;
+
     let deck = [];
     let value = 0
-        for(let i = 0; i < 52; i++){
-            if(i < 4) {
-                value = 11
-            } else if(i < 8) {
-                value = 2
-            } else if(i < 12) {
-                value = 3
-            } else if (i < 16) {
-                value = 4
-            } else if (i < 20) {
-                value = 5
-            } else if (i < 24) {
-                value = 6
-            } else if (i < 28) {
-                value = 7
-            } else if (i < 32) {
-                value = 8
-            } else if (i < 36) {
-                value = 9
-            } else if (i < 52) {
-                value = 10
-            }
-            deck.push({value: value, cardImg: "images/" + i + ".jpg"});
+    for(let i = 0; i < 52; i++){
+        if(i < 4) {
+            value = 11
+        } else if(i < 8) {
+            value = 2
+        } else if(i < 12) {
+            value = 3
+        } else if (i < 16) {
+            value = 4
+        } else if (i < 20) {
+            value = 5
+        } else if (i < 24) {
+            value = 6
+        } else if (i < 28) {
+            value = 7
+        } else if (i < 32) {
+            value = 8
+        } else if (i < 36) {
+            value = 9
+        } else if (i < 52) {
+            value = 10
+        }
+        deck.push({value: value, cardImg: "images/" + i + ".jpg"});
     };
-        
-    console.log(deck);
-    console.log(deck.length);
-
-    // function getRandomCard(min, max) {
-    //     randomCard = Math.floor(Math.random() * (max - min + 1)) + min;
-    //     console.log(randomCard);
-    //     return randomCard;
-    // };
 
     function shuffleDeck(deck) {
         let i = 0;
@@ -46,8 +42,7 @@ $(document).ready(function() {
                     temporaryValue = deck[i];
                     deck[i] = deck[randomIndex];
                     deck[randomIndex] = temporaryValue;
-            }
-        // getRandomCard(0, 51);            
+            }           
     };
 
     function newDeck() {
@@ -61,15 +56,14 @@ $(document).ready(function() {
     $('.dealBtn').on('click', function(evt) {
         shuffleDeck(deck);
         evt.preventDefault();
-            $('#dealerCard-1').append('<img src = "images/Red_back.jpg">');
-            $('#dealerCard-2').append("<div><img src = '" + deck[1].cardImg + "' /></div>");
-            $('#playerCard-1').append("<div><img src = '" + deck[2].cardImg + "' /></div>");
-            $('#playerCard-2').append("<div><img src = '" + deck[0].cardImg + "' /></div>");
+            $('#dealerCard-1').append("<div><img src = '" + deck[Math.floor(Math.random() * deck.length)].cardImg + "' /></div>");
+                                    //  ('<img src = "images/Red_back.jpg">');                                                                                                  
+            $('#dealerCard-2').append("<div><img src = '" + deck[Math.floor(Math.random() * deck.length)].cardImg + "' /></div>");
+            $('#playerCard-1').append("<div><img src = '" + deck[Math.floor(Math.random() * deck.length)].cardImg + "' /></div>");
+            $('#playerCard-2').append("<div><img src = '" + deck[Math.floor(Math.random() * deck.length)].cardImg + "' /></div>");
         
         newDeck();
     });
-
-
 
 
 
